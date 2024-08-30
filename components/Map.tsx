7f377e92d180ba2002604563e0a3f76b0adc2a60
Rohit <rr789451@gmail.com>
2024-08-31 00:42:46 +0530

    Feat: Confirm Ride Screen

--- components/Map.tsx
+++ components/Map.tsx
@@ -14,7 +14,8 @@ const drivers = [
         "profile_image_url": "https://ucarecdn.com/dae59f69-2c1f-48c3-a883-017bcf0f9950/-/preview/1000x666/",
         "car_image_url": "https://ucarecdn.com/a2dc52b2-8bf7-4e49-9a36-3ffb5229ed02/-/preview/465x466/",
         "car_seats": 4,
-        "rating": "4.80"
+        "rating": "4.80",
+        "time": "5"
     },
     {
         "id": "2",
@@ -23,7 +24,8 @@ const drivers = [
         "profile_image_url": "https://ucarecdn.com/6ea6d83d-ef1a-483f-9106-837a3a5b3f67/-/preview/1000x666/",
         "car_image_url": "https://ucarecdn.com/a3872f80-c094-409c-82f8-c9ff38429327/-/preview/930x932/",
         "car_seats": 5,
-        "rating": "4.60"
+        "rating": "4.60",
+        "time": "5"
     },
     {
         "id": "3",
@@ -32,7 +34,8 @@ const drivers = [
         "profile_image_url": "https://ucarecdn.com/0330d85c-232e-4c30-bd04-e5e4d0e3d688/-/preview/826x822/",
         "car_image_url": "https://ucarecdn.com/289764fb-55b6-4427-b1d1-f655987b4a14/-/preview/930x932/",
         "car_seats": 4,
-        "rating": "4.70"
+        "rating": "4.70",
+        "time": "5"
     },
     {
         "id": "4",
@@ -41,7 +44,8 @@ const drivers = [
         "profile_image_url": "https://ucarecdn.com/fdfc54df-9d24-40f7-b7d3-6f391561c0db/-/preview/626x417/",
         "car_image_url": "https://ucarecdn.com/b6fb3b55-7676-4ff3-8484-fb115e268d32/-/preview/930x932/",
         "car_seats": 4,
-        "rating": "4.90"
+        "rating": "4.90",
+        "time": "5"
     }
 ]
 
@@ -64,6 +68,9 @@ const Map = () => {
     })
 
     useEffect(() => {
+
+        setDrivers(drivers);
+
         if(Array.isArray(drivers)) {
             if(!userLatitude || !userLongitude) return;
 
