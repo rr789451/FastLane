ff97643f41c75c8a77f75bcdab023c87756b20ab
Rohit <rr789451@gmail.com>
2024-09-01 16:58:48 +0530

    Feat: Profile Screen

--- app/(root)/(tabs)/history.tsx
+++ app/(root)/(tabs)/history.tsx
@@ -12,41 +12,41 @@ const History = () => {
     const { data : recentRides, loading } = useFetch<Ride[]>(`/(api)/ride/${user?.id}`)
 
     return(
-        <SafeAreaView>
-            <FlatList 
-        data={recentRides}
-        renderItem={({item}) => <RideCard ride={item}/>}
-        className='px-5'
-        keyboardShouldPersistTaps="handled"
-        contentContainerStyle={{
-          paddingBottom: 100,
-        }}
-        ListEmptyComponent={() => (
-          <View className='flex flex-col items-center justify-center'>
-            {!loading ? (
-              <>
-                <Image 
-                  source={images.noResult}
-                  className='w-40 h-40'
-                  alt='No recent rides found'
-                  resizeMode='contain'
-                />
-                <Text className='text-sm'>No recent rides found!</Text>
-              </>
-            ) : 
-            
-              <ActivityIndicator size="small" color="#000" />
-            
-            }
-          </View>
-        )}  
-        ListHeaderComponent={() => (
-          <>
-            <Text className="text-2xl font-JakartaBold my-5">All Rides</Text>
-          </>
-        )}
-      />
-        </SafeAreaView>
+      <SafeAreaView>
+        <FlatList 
+          data={recentRides}
+          renderItem={({item}) => <RideCard ride={item}/>}
+          className='px-5'
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{
+            paddingBottom: 100,
+          }}
+          ListEmptyComponent={() => (
+            <View className='flex flex-col items-center justify-center'>
+              {!loading ? (
+                <>
+                  <Image 
+                    source={images.noResult}
+                    className='w-40 h-40'
+                    alt='No recent rides found'
+                    resizeMode='contain'
+                  />
+                  <Text className='text-sm'>No recent rides found!</Text>
+                </>
+              ) : 
+              
+                <ActivityIndicator size="small" color="#000" />
+              
+              }
+            </View>
+          )}  
+          ListHeaderComponent={() => (
+            <>
+              <Text className="text-2xl font-JakartaBold my-5">All Rides</Text>
+            </>
+          )}
+        />
+      </SafeAreaView>
     )
 }
 
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,73 @@
-import { Text, View } from "react-native"
+import InputField from "@/components/InputField";
+import { icons } from "@/constants";
+import { useUser } from "@clerk/clerk-expo";
+import { Image, ScrollView, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
+
+    const { user } = useUser();
+
     return(
-        <SafeAreaView>
-            <Text>Profile</Text>
-        </SafeAreaView>
+            <ScrollView className="px-5 bg-neutral-100">
+            <Text className="text-2xl font-JakartaBold mt-20">Your Profile</Text>
+            <SafeAreaView className="flex flex-col items-center justify-center">
+                <View className="w-40 h-40 bg-white rounded-full mt-[-40px]">
+                    <Image 
+                        source={{ uri : user?.imageUrl }}
+                        className="w-[150px] h-[150px] rounded-full mx-auto my-auto"
+                        resizeMode="contain"
+                    />
+                </View>
+                <View className="mt-10 mb-20 w-full h-70 bg-white rounded-xl relative">
+                    <InputField 
+                        label="First Name"
+                        labelStyle="text-general-200 font-JakartaMedium mx-4"
+                        containerStyle="mx-3"
+                        inputStyle="text-md"
+                        editable={false}
+                    >
+                        {user?.firstName}
+                    </InputField>
+                    <InputField 
+                        label="Last Name"
+                        labelStyle="text-general-200 font-JakartaMedium mx-4"
+                        containerStyle="mx-3"
+                        inputStyle="text-md"
+                        editable={false}
+                    >
+                        {user?.lastName || 'Reddy'}
+                    </InputField>
+                    <InputField 
+                        label="Email"
+                        labelStyle="text-general-200 font-JakartaMedium mx-4"
+                        containerStyle="mx-3"
+                        inputStyle="text-md"
+                        editable={false}
+                    >
+                        {user?.emailAddresses[0].emailAddress}
+                    </InputField>
+                    <InputField 
+                        label="Email status"
+                        labelStyle="text-general-200 font-JakartaMedium mx-4"
+                        containerStyle="mx-3"
+                        inputStyle={`text-md ${ user?.hasVerifiedEmailAddress ? 'text-green-500' : 'text-red-500' }`}
+                        editable={false}
+                    >
+                        {user?.hasVerifiedEmailAddress ? 'Verified' : 'Not Verified'}
+                    </InputField>
+                    <InputField 
+                        label="Phone Number"
+                        labelStyle="text-general-200 font-JakartaMedium mx-4"
+                        containerStyle="mx-3"
+                        inputStyle="text-md"
+                        editable={false}
+                    >
+                        {user?.primaryPhoneNumber?.phoneNumber || '+916281547906'}
+                    </InputField>
+                </View>
+                </SafeAreaView>
+            </ScrollView>
     )
 }
 
